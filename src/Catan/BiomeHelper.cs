5cfb448545a14ab8fab86b3e3a1b51287a10f490
Owen Mellema <owen.mellema@cerner.com>
2021-01-29 10:19:07 -0600

    Create BiomeHelper evaluate

---
+++ src/Catan/BiomeHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Catan
+{
+	/**
+	 * Helps with creating biome maps. In biome maps, every tile touches at least one other tile of the same type.
+	 */
+	public class BiomeHelper
+	{
+		/**
+		 * Returns true if the coordinate meets all constraints.
+		 */
+		public static bool evaluateCoordinate(Board board, HexagonalCoordinate coord)
+        {
+			Tile? current = board.getTile(coord);
+			if (current == null)
+            {
+				return true;
+            }
+			else if (current == Tile.DESERT)
+            {
+				return true;
+            }
+
+			List<Tile?> neighborTiles = new List<Tile?>();
+
+			foreach (HexagonalCoordinate neighborCoord in coord.getNeighbors())
+			{
+				if (Board.isOnBoard(neighborCoord))
+                {
+					neighborTiles.Add(board.getTile(neighborCoord));
+                }
+            }
+
+			return neighborTiles.Contains(current);
+        }
+	}
+}
+
---
+++ test/Catan.Tests/BiomeHelperTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Catan.Tests
+{
+    public class BiomeHelperTest
+    {
+        public class EvaluatorTest
+        {
+            [Theory]
+            [InlineData(0, 0, 0, 1, 0, -1)]
+            [InlineData(0, 0, 0, -1, 0, 1)]
+            [InlineData(0, 0, 0, 0, 1, -1)]
+            [InlineData(0, 0, 0, 0, -1, 1)]
+            [InlineData(0, 0, 0, 1, -1, 0)]
+            [InlineData(0, 0, 0, -1, 1, 0)]
+            [InlineData(2, 0, -2, 1, 0, -1)]
+            [InlineData(2, 0, -2, 2, -1, -1)]
+            [InlineData(2, 0, -2, 1, 1, -2)]
+            public void returnsTrueIfHasOneSurrounding(int x, int y, int z, int ox, int oy, int oz)
+            {
+                Board board = new Board();
+                board.setTile(new HexagonalCoordinate(x, y, z), Tile.CLAY);
+                board.setTile(new HexagonalCoordinate(ox, oy, oz), Tile.CLAY);
+                Assert.True(BiomeHelper.evaluateCoordinate(board, new HexagonalCoordinate(x, y, z)));
+            }
+        }
+    }
+}
